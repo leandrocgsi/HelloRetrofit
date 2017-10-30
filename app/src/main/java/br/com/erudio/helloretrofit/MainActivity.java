7bb63c08d27883ab9f4547252db36be34372d9df
leandrocgsi <leandrocgsi@gmail.com>
2017-10-30 10:19:16 -0200

    Consuming services

--- app/src/main/java/br/com/erudio/helloretrofit/MainActivity.java
+++ app/src/main/java/br/com/erudio/helloretrofit/MainActivity.java
@@ -2,12 +2,77 @@ package br.com.erudio.helloretrofit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import br.com.erudio.helloretrofit.data.model.Item;
+import br.com.erudio.helloretrofit.data.model.SOAnswersResponse;
+import br.com.erudio.helloretrofit.data.remote.AnswersAdapter;
+import br.com.erudio.helloretrofit.data.remote.ApiUtils;
+import br.com.erudio.helloretrofit.data.remote.SOService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AnswersAdapter mAdapter;
+    private RecyclerView mRecyclerView;
+    private SOService mService;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate( savedInstanceState );
+        setContentView(R.layout.activity_main );
+        mService = ApiUtils.getSOService();
+        mRecyclerView = (RecyclerView) findViewById(R.id.rv_answers);
+        mAdapter = new AnswersAdapter(this, new ArrayList<Item>(0), new AnswersAdapter.PostItemListener() {
+
+            @Override
+            public void onPostClick(long id) {
+                Toast.makeText(MainActivity.this, "Post id is" + id, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setHasFixedSize(true);
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        mRecyclerView.addItemDecoration(itemDecoration);
+
+        loadAnswers();
+    }
+
+    public void loadAnswers() {
+        mService.getAnswers().enqueue(new Callback<SOAnswersResponse>() {
+            @Override
+            public void onResponse(Call<SOAnswersResponse> call, Response<SOAnswersResponse> response) {
+
+                if(response.isSuccessful()) {
+                    mAdapter.updateAnswers(response.body().getItems());
+                    Log.d("MainActivity", "posts loaded from API");
+                }else {
+                    int statusCode  = response.code();
+                    // handle request errors depending on status code
+                }
+            }
+
+            @Override
+            public void onFailure(Call<SOAnswersResponse> call, Throwable t) {
+                showErrorMessage();
+                Log.d("MainActivity", "error loading from API");
+
+            }
+        });
+    }
+
+    public void showErrorMessage() {
+        Toast.makeText(this, "Error loading posts", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/br/com/erudio/helloretrofit/data/remote/ApiUtils.java
@@ -0,0 +1,11 @@
+package br.com.erudio.helloretrofit.data.remote;
+
+public class ApiUtils {
+
+    public static final String BASE_URL = "https://api.stackexchange.com/2.2/";
+
+    public static SOService getSOService() {
+        return RetrofitClient.getClient(BASE_URL).create(SOService.class);
+    }
+
+}
