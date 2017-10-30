b54f736558ccf06aa4ad6e36d79aa9cfd575d6c3
leandrocgsi <leandrocgsi@gmail.com>
2017-10-30 11:34:31 -0200

    Adding ReativeX support

--- HelloRetroitCRUD/app/src/main/java/br/com/erudio/helloretroitcrud/data/remote/RetrofitClient.java
+++ HelloRetroitCRUD/app/src/main/java/br/com/erudio/helloretroitcrud/data/remote/RetrofitClient.java
@@ -1,6 +1,7 @@
 package br.com.erudio.helloretroitcrud.data.remote;
 
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
@@ -8,7 +9,13 @@ public class RetrofitClient {
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient(String baseUrl) {
-        if (retrofit == null) retrofit = new Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(GsonConverterFactory.create()).build();
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(baseUrl)
+                    .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
         return retrofit;
     }
 }
